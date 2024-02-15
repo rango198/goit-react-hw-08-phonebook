696c6c48923f48f57b6b2cba6a29f551eb127276
rango198 <137562811+rango198@users.noreply.github.com>
2024-02-15 08:28:30 +0200

    noWorked

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import UserMenu from 'components/UserMenu/UserMenu';
 import { authSelector } from 'components/redux/Auth/authSelector';
